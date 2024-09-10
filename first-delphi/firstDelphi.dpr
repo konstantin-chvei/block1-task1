8cb64cc952c5bcf6ed34ee97eca97cfdffc755d8
konstantin-chvei <chveikonstantin@gmail.com>
2024-09-10 23:20:22 +0300

    Update firstDelphi.dpr

--- first-delphi/firstDelphi.dpr
+++ first-delphi/firstDelphi.dpr
@@ -4,68 +4,66 @@ program firstDelphi;
 
 {$R *.res}
 
-uses
+Uses
   System.SysUtils;
 
-var
-    firstSide, secondSide, thirdSide:integer;
-    input:string;
-    isCorrect:boolean;
-    firstSideSqr, secondSideSqr, thirdSideSqr:Int64;
+Var
+    FirstSide, SecondSide, ThirdSide:Integer;
+    Input:String;
+    IsCorrect:Boolean;
+    FirstSideSqr, SecondSideSqr, ThirdSideSqr:Int64;
 
-begin
+Begin
 
-    isCorrect := false;
-    while(not isCorrect) do begin
-    writeln('Введите значение первой стороны треугольника не певышающее 1000000000 ');
-    readln(input);
-    isCorrect := TryStrToInt(input, firstSide);
+    IsCorrect := False;
+    While Not IsCorrect Do
+    Begin
+        Writeln('Введите значение первой стороны треугольника не певышающее 1000000000 ');
+        Readln(Input);
+        IsCorrect := TryStrToInt(Input, FirstSide);
 
-    if(isCorrect and (firstSide > 1000000000)) then begin
-        writeln('Значение не должно превышать 1000000000, повторите ввод')
-    end;
+        If IsCorrect And (FirstSide > 1000000000) Then
+            Writeln('Значение не должно превышать 1000000000, повторите ввод');
 
-    if(not isCorrect) then
-        writeln('сторона должна быть целым числом, повторите ввод')
-    end;
+        If Not IsCorrect Then
+            Writeln('сторона должна быть целым числом, повторите ввод');
+    End;
 
-    isCorrect := false;
-    while(not isCorrect) do begin
-    writeln('Введите значение второй стороны треугольника не певышающее 1000000000 ');
-    readln(input);
-    isCorrect := TryStrToInt(input, secondSide);
+    IsCorrect := False;
+    While Not IsCorrect Do
+    Begin
+        Writeln('Введите значение второй стороны треугольника не певышающее 1000000000 ');
+        Readln(Input);
+        IsCorrect := TryStrToInt(Input, SecondSide);
 
-    if(isCorrect and (secondSide > 1000000000)) then begin
-        writeln('Значение не должно превышать 1000000000, повторите ввод')
-    end;
+        If IsCorrect And (SecondSide > 1000000000) Then
+            Writeln('Значение не должно превышать 1000000000, повторите ввод');
 
-    if(not isCorrect) then
-        writeln('сторона должна быть целым числом, повторите ввод')
-    end;
+        If Not IsCorrect Then
+            Writeln('сторона должна быть целым числом, повторите ввод');
+    End;
 
-    isCorrect := false;
-    while(not isCorrect) do begin
-    writeln('Введите значение третьей стороны треугольника не певышающее 1000000000 ');
-    readln(input);
-    isCorrect := TryStrToInt(input, thirdSide);
+    IsCorrect := False;
+    While Not IsCorrect Do
+    Begin
+        Writeln('Введите значение третьей стороны треугольника не певышающее 1000000000 ');
+        Readln(input);
+        IsCorrect := TryStrToInt(input, thirdSide);
 
-    if(isCorrect and (thirdSide > 1000000000)) then begin
-        writeln('Значение не должно превышать 1000000000, повторите ввод')
-    end;
+        If IsCorrect And (ThirdSide > 1000000000) Then
+        Writeln('Значение не должно превышать 1000000000, повторите ввод');
 
-    if(not isCorrect) then
-        writeln('сторона должна быть целым числом, повторите ввод')
-    end;
+        If Not IsCorrect Then
+        Writeln('сторона должна быть целым числом, повторите ввод');
+    End;
 
-    firstSideSqr  := firstSide  * firstSide;
-    secondSideSqr := secondSide * secondSide;
-    thirdSideSqr  := thirdSide  * thirdSide;
+    FirstSideSqr  := FirstSide  * FirstSide;
+    SecondSideSqr := SecondSide * SecondSide;
+    ThirdSideSqr  := ThirdSide  * ThirdSide;
 
-    if((firstSideSqr = secondSideSqr + thirdSideSqr) or
-    (secondSideSqr = firstSideSqr + thirdSideSqr)    or
-    (thirdSideSqr = firstSideSqr + secondSideSqr))   then
-    writeln('Треугольник прямоугольный') else
-    writeln('Треугольник не прямоугольный');
-
-    readln;
-end.
+    If (FirstSideSqr = SecondSideSqr + ThirdSideSqr) Or (SecondSideSqr = FirstSideSqr + ThirdSideSqr) Or (ThirdSideSqr = FirstSideSqr + SecondSideSqr) Then
+        Writeln('Треугольник прямоугольный')
+    Else
+        Writeln('Треугольник не прямоугольный');
+    Readln;
+End.
