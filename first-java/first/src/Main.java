5b50319719921b498f09113a1230080a4758fefc
konstantin-chvei <chveikonstantin@gmail.com>
2024-09-11 17:21:05 +0300

    Update Main.java

--- first-java/first/src/Main.java
+++ first-java/first/src/Main.java
@@ -7,20 +7,22 @@ public class Main {
         int secondSide;
         int thirdSide;
 
+        int value;
+
         int firstSideSqr;
         int secondSideSqr;
         int thirdSideSqr;
-        int value;
 
         boolean isCorrect;
 
-        firstSide  = 0;
+        firstSide = 0;
         secondSide = 0;
-        thirdSide  = 0;
+        thirdSide = 0;
+        value = 0;
 
-        firstSideSqr  = 0;
+        firstSideSqr = 0;
         secondSideSqr = 0;
-        thirdSideSqr  = 0;
+        thirdSideSqr = 0;
 
         isCorrect = false;
         System.out.println("Дано:\tДаны длины сторон треугольника \nЗадача:\tОпределить, является ли данный треугольник прямоугольным.\n\n");
@@ -29,21 +31,21 @@ public class Main {
         while (!isCorrect) {
             if (scanner.hasNextInt()) {
                 value = scanner.nextInt();
-                if (value < 0 ) {
+                if (value < 0) {
                     isCorrect = false;
                     System.out.println("сторона не может быть отрицательной, повторите ввод");
-                    scanner.next();
-                } else if (value > 1000000001) {
+                    scanner.nextLine();
+                } else if (value > 1000000000) {
                     isCorrect = false;
                     System.out.println("сторона не может превышать 1000000000, повторите ввод");
-                    scanner.next();
+                    scanner.nextLine();
                 } else {
                     firstSide = value;
                     isCorrect = true;
                 }
             } else {
                 isCorrect = false;
-                System.out.println("сторона не может превышать 1000000000 или не являться целым числом, повторите ввод");
+                System.out.println("сторона не может превышать 1000000000 или не являться числом, повторите ввод");
                 scanner.next();
             }
         }
@@ -53,21 +55,21 @@ public class Main {
         while (!isCorrect) {
             if (scanner.hasNextInt()) {
                 value = scanner.nextInt();
-                if (value < 0 ) {
+                if (value < 0) {
                     isCorrect = false;
                     System.out.println("сторона не может быть отрицательной, повторите ввод");
-                    scanner.next();
-                } else if (value > 1000000001) {
+                    scanner.nextLine();
+                } else if (value > 1000000000) {
                     isCorrect = false;
                     System.out.println("сторона не может превышать 1000000000, повторите ввод");
-                    scanner.next();
+                    scanner.nextLine();
                 } else {
                     secondSide = value;
                     isCorrect = true;
                 }
             } else {
                 isCorrect = false;
-                System.out.println("сторона не может превышать 1000000000 или не являться целым числом, повторите ввод");
+                System.out.println("сторона не может превышать 1000000000 или не являться числом, повторите ввод");
                 scanner.next();
             }
         }
@@ -77,34 +79,38 @@ public class Main {
         while (!isCorrect) {
             if (scanner.hasNextInt()) {
                 value = scanner.nextInt();
-                if (value < 0 ) {
+                if (value < 0) {
                     isCorrect = false;
                     System.out.println("сторона не может быть отрицательной, повторите ввод");
-                    scanner.next();
-                } else if (value > 1000000001) {
+                    scanner.nextLine();
+                } else if (value > 1000000000) {
                     isCorrect = false;
                     System.out.println("сторона не может превышать 1000000000, повторите ввод");
-                    scanner.next();
+                    scanner.nextLine();
                 } else {
                     thirdSide = value;
                     isCorrect = true;
                 }
             } else {
                 isCorrect = false;
-                System.out.println("сторона не может превышать 1000000000 или не являться целым числом, повторите ввод");
+                System.out.println("сторона не может превышать 1000000000 или не являться числом, повторите ввод");
                 scanner.next();
             }
         }
         scanner.close();
 
-        firstSideSqr  = firstSide * firstSide;
+        firstSideSqr = firstSide * firstSide;
         secondSideSqr = secondSide * secondSide;
-        thirdSideSqr  = thirdSide * thirdSide;
+        thirdSideSqr = thirdSide * thirdSide;
 
-        if ((firstSideSqr == secondSideSqr + thirdSideSqr) | (secondSideSqr == thirdSideSqr + firstSideSqr) | (thirdSideSqr == firstSideSqr + secondSideSqr)){
-            System.out.println("треугольник прямоугольный");
+        if (firstSide < (secondSide + thirdSide) && secondSide < (firstSide + thirdSide) && thirdSide < (secondSide + firstSide) && (firstSide != 0 && secondSide != 0 && thirdSide != 0)) {
+            if ((firstSideSqr == secondSideSqr + thirdSideSqr) | (secondSideSqr == thirdSideSqr + firstSideSqr) | (thirdSideSqr == firstSideSqr + secondSideSqr)) {
+                System.out.println("треугольник прямоугольный");
+            } else {
+                System.out.println("треугольник не прямоугольный");
+            }
         } else {
-            System.out.println("треугольник не прямоугольный");
+            System.out.println("треугольник не существует");
         }
     }
 }
